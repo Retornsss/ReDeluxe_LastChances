c83824dc5fd91bcccdda362cf15b4449dcb70a8f
Osmar <oarm.retorns@gmail.com>
2025-10-19 13:45:13 -0600

    Correcciones

--- src/client/java/re/deluxe/mixin/MixinGameRenderer.java
+++ src/client/java/re/deluxe/mixin/MixinGameRenderer.java
@@ -1,35 +1,43 @@
 package re.deluxe.mixin;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.LightTexture;
+import net.minecraft.util.profiling.Profiler;
+import net.minecraft.util.profiling.ProfilerFiller;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.GameRenderer;
-import net.minecraft.client.renderer.LightTexture;
-
 import re.deluxe.Darkness;
 import re.deluxe.LightmapAccess;
 
 @Mixin(GameRenderer.class)
 public class MixinGameRenderer {
-    @Shadow
+    @Shadow @Final
     private Minecraft minecraft;
-    @Shadow
+
+    @Shadow @Final
     private LightTexture lightTexture;
 
-    @Inject(method = "renderLevel", at = @At(value = "HEAD"))
+    @Inject(method = "renderLevel", at = @At("HEAD"))
     private void onRenderLevel(DeltaTracker deltaTracker, CallbackInfo ci) {
         final LightmapAccess lightmap = (LightmapAccess) lightTexture;
 
         if (lightmap.darkness_isDirty()) {
-            minecraft.getProfileKeyPairManager().push("lightTex");
+            ProfilerFiller profilerFiller = Profiler.get();
+            profilerFiller.push("lightTex");
+
+            // Usa el delta desde DeltaTracker
+            float tickDelta = deltaTracker.getGameTimeDeltaPartialTick(false);
+
             Darkness.updateLuminance(tickDelta, minecraft, (GameRenderer) (Object) this, lightmap.darkness_prevFlicker());
-            minecraft.getProfileKeyPairManager().pop();
+
+            profilerFiller.pop();
         }
     }
 }
--- src/client/java/re/deluxe/mixin/MixinLightTexture.java
+++ src/client/java/re/deluxe/mixin/MixinLightTexture.java
@@ -1,5 +1,6 @@
 package re.deluxe.mixin;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,7 +20,7 @@ public class MixinLightTexture implements LightmapAccess {
     @Shadow
     private DynamicTexture lightTexture;
     @Shadow
-    private float blockLightRedFlicker; // AAAAAAAA
+    private float blockLightRedFlicker;
     @Shadow
     private boolean updateLightTexture;
 
